4594d09c55af95bbe47fd760dcf0e4c71cc3345f
SantoCyber <santocyber@gmail.com>
2016-03-06 11:11:50 -0500

    Update index-pandora.php

--- index-pandora.php
+++ index-pandora.php
@@ -1,6 +1,8 @@
 <!-- 
-sudo chmod a+rw /dev/ttyACM0
+sudo chmod a+rw /dev/ttyUSB0
 "r+b" seria permissão para leitura e escrita na porta.
+
+
 
 $port=fopen($portAdress,"r+b");
 
@@ -11,19 +13,20 @@ O conteudo deste script (hugreen.solar) esta sob licenca Creative Commons  -->
         <meta http-equiv="Content-Language" content="pt-br" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="http://x.mirako.org" />
-        <title>..::: CONTROLE DE ROBO PANDORA :::..</title>
+    <title>Proteus Robot Hugreen.Solar</title>
     </head>
-
     <body>
         <body bgcolor="#D4D2D2">
         
-        <h1>Pandora movel com Arduino/RaspBerry/Apache/PHP/Shell/LinuxSerial</h1>
+<h1>
+Proteus Robot Hugreen.Solar
+</h1>
 
 <?php
 $address2 = $_SERVER['SERVER_ADDR'];
 $address =  str_replace(':8081', '', $_SERVER['HTTP_HOST']);
 
-if( $address == '192.168.5.240' ) {
+if( $address == '192.168.1.70') {
   $domain = $_SERVER['SERVER_ADDR'];
 } 
 else {
@@ -33,14 +36,13 @@ $domain = str_replace(':8081', '', $_SERVER['HTTP_HOST']);
 ?>
 
 
-<iframe frameborder="0" marginheight="0" marginwidth="0" scrolling="no"   src = "http://<?php echo $domain ?>:8081"></iframe>
-
+<iframe frameborder="0" marginheight="0" marginwidth="0" scrolling="no"   src = "http://panfilo.noip.us:8081" width="320" height="240" ></iframe>
 <br><br>
 
 <?php
 
 
-    $port = fopen("/dev/ttyACM0", "r+b");
+    $port = fopen("/dev/ttyUSB0", "r+b");
 
       if ($_POST['situacao']=="Acionar")
       {
@@ -73,11 +75,11 @@ echo fgets($port);
 
       if ($_POST['situacao']=="frente")
       {
-      fwrite($port, "s");
+      fwrite($port, "w");
       }
       if ($_POST['situacao']=="reh")
       {
-      fwrite($port, "w");
+      fwrite($port, "s");
       }
       if ($_POST['situacao']=="direita")
       {
@@ -93,7 +95,7 @@ echo fgets($port);
       }
       if ($_POST['situacao']=="bibi")
       {
-      fwrite($port, "i");
+      fwrite($port, "b");
       }
 
 
@@ -107,8 +109,34 @@ fclose($port);
 <table border="0" cellpadding="0" cellspacing="0" style="width: 12%;">
 <tbody>
 <tr>
-        <td>    
 
+<form method="POST" action="index.php">
+
+        <input type="hidden" value="frente" name="situacao">
+        <input type="submit" value="Frente" name="frente" style="border-color:black; border-style: solid; border-width: 1; background-color: green; color: white">
+</form>
+
+<form method="POST" action="index.php">
+        <input type="hidden" value="esquerda" name="situacao">
+        <input type="submit" value="Esquerda" name="esquerda" style="border-color:black; border-style: solid; border-width: 1; background-color: green; color: white"></form>
+
+<form method="POST" action="index.php">
+
+        <input type="hidden" value="direita" name="situacao">
+        <input type="submit" value="Direita" name="direita" style="border-color:black; border-style: solid; border-width: 1; background-color: green; color: white">
+        </form>
+
+<form method="POST" action="index.php">
+        <input type="hidden" value="reh" name="situacao">
+        <input type="submit" value="Reh" name="reh" style="border-color:black; border-style: solid; border-width: 1; background-color: green; color: white">
+</form>
+</td>
+</tr>
+<tr></tr>
+<br>
+
+        <td>    
+<br><br>
 <!-- LASER -->
 
 <form method="POST" action="index.php">
@@ -150,58 +178,8 @@ green; color: white">
         <input type="submit" value="BiBi" name="bibi" style="border-color:black; border-style: solid; border-width: 1; background-color: 
 green; color: white">
 </form>
-</td>
 
 
-<form method="POST" action="index.php">
-
-        <input type="hidden" value="frente" name="situacao">
-        <input type="submit" value="Frente" name="frente" style="border-color:black; border-style: solid; border-width: 1; background-color: green; color: white">
-</form>
-
-<form method="POST" action="index.php">
-        <input type="hidden" value="esquerda" name="situacao">
-        <input type="submit" value="Esquerda" name="esquerda" style="border-color:black; border-style: solid; border-width: 1; background-color: green; color: white"></form>
-
-<form method="POST" action="index.php">
-
-        <input type="hidden" value="direita" name="situacao">
-        <input type="submit" value="Direita" name="direita" style="border-color:black; border-style: solid; border-width: 1; background-color: green; color: white">
-        </form>
-
-<form method="POST" action="index.php">
-        <input type="hidden" value="reh" name="situacao">
-        <input type="submit" value="Reh" name="reh" style="border-color:black; border-style: solid; border-width: 1; background-color: green; color: white">
-</form>
-</td>
-</tr>
-
-<td>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-<!-- DESATIVA RELE -->
-
-<form method="POST" action="index.php">
-
-        <input type="hidden" value="reset" name="situacao">
-        <input type="submit" value="RESET" name="reset" style="border-color:black; border-style: solid; border-width: 1; background-color: 
-red; color: white">
-
-
-    
-</form>
-        </td>
-    </tr>
-</tbody>
-</table>
-
-        <p>OBS: Rodando em localhost.</p>
         <p><a href="http://x.mirako.org" target="_blank">http://x.mirako.org</a></p>
         <p>v.0.9.3</p>
     </body>
